65042db8ae1661360ea59c8bbfc1787d8eb4d3d7
ryaba <ryabayeah@gmail.com>
2020-11-22 20:44:51 +0700

    lab5

---
+++ Lab_5/lab5.js
@@ -0,0 +1,10 @@
+showBlock = id => {
+    document.getElementById(id).style.display = 'block';
+    document.getElementById('background').style.display = 'block';
+  };
+
+
+hideBlock = () => {
+    Array.from(document.querySelectorAll('.new_block')).forEach(item => item.style.display = 'none')
+    document.getElementById('background').style.display = 'none';
+}
