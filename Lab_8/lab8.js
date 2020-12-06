be3fc4baeabb5a468cc73d41a1d064155532432a
Anastasia Abazovskaya <ryabayeah@gmail.com>
2020-12-07 01:52:42 +0700

    lab8

--- Lab_8/lab8.js
+++ Lab_8/lab8.js
@@ -1,12 +1,46 @@
 AddRow = () => {
     var row = document.createElement('div');
     row.innerHTML = `
-    <input type="text"><input type="text">
-    <button onclick="" >↑</button><button onclick="" >↓</button><button onclick="DeleteRow(this)" >x</button>
+    <input class = "one" type="text"><input class = "two" type="text"><button 
+    onclick="SwapUp(this)" >↑</button><button
+    onclick="SwapDown(this)" >↓</button><button
+    onclick="DeleteRow(this)" >x</button>
   `;
-  document.querySelector('container').appendChild(row);
+    document.querySelector('#container').appendChild(row);
 }
 
 DeleteRow = (row) => {
-    row.parentNode.remove();}
+    row.parentNode.remove();
+}
+
+SwapUp = (elem) => {
+    var parentRow = elem.parentElement;
+    var previousPow = parentRow.previousElementSibling;
+    if (previousPow)
+        parentRow.after(previousPow);
+
+
+}
+SwapDown = (elem) => {
+    var parentRow = elem.parentElement;
+    var nextRow = parentRow.nextElementSibling;
+    if (nextRow)
+        parentRow.before(nextRow);
+
+}
+
+SaveValues = () => {
+    var firstInputs = document.querySelectorAll('.one');
+    var secondInputs = document.querySelectorAll('.two');
+
+    let list = '';
+
+    for (i = 0; i < firstInputs.length; i++) {
+        list += `"${firstInputs[i].value
+            }":"${secondInputs[i].value
+            }", `;
+    }
+
+    document.querySelector("text").innerHTML = `{${list.slice(0, -2)}}`;
+}
 AddRow();
