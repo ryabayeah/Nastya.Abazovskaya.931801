51f4697f2987fbd104831eb4915c9311fc3e5e76
Anastasia Abazovskaya <ryabayeah@gmail.com>
2020-12-27 15:58:34 +0700

    fix 7 lab

--- Lab_7/lab7.js
+++ Lab_7/lab7.js
@@ -9,11 +9,15 @@ showFigure = class_id =>{
         div.setAttribute('class',class_id);
         div.style.top = String(Math.random()*700+1) + 'px';
         div.style.left = String(Math.random()*1000+1) + 'px';
+        div.addEventListener('click',changeColorForTriangle);
+        div.addEventListener('dblclick',dissapearFigure);
 
     if ( class_id == 'square' )
         {
             var size = String(Math.random()*125+25) + 'px';
             div.style.width = div.style.height = size;
+            div.addEventListener('click',changeColor);
+            div.addEventListener('dblclick',dissapearFigure);
         }
 
     if ( class_id == 'circle' )
@@ -27,6 +31,15 @@ showFigure = class_id =>{
         document.querySelector("main").appendChild(div);
 
     }
+}
+
+changeColorForTriangle = (event) =>
+{
+    r = Math.floor(Math.random() * (256)),
+    g = Math.floor(Math.random() * (256)),
+    b = Math.floor(Math.random() * (256)),
+    color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    event.target.style.borderBottomColor = color;
 }
 
 changeColor = (event) =>
